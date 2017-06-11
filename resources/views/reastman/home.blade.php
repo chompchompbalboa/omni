b2f36a8fe53cd467e3c6cbf2c99c7a9449389047
Rocky Eastman Jr <eastmanrjr@gmail.com>
2017-06-11 15:05:11 -0700

    reastman.com - Updated content

--- resources/views/reastman/home.blade.php
+++ resources/views/reastman/home.blade.php
@@ -51,18 +51,19 @@
 									Wouldn't it be great if there was someone to take care of it for you?
 								</p>
 								<ul class="actions">
-									<li><a href="#" class="button">Let me be that someone</a></li>
+									<li><a href="#four" class="button">Let me be that someone</a></li>
 								</ul>
 							</div>
 							<div class="secondary">
 								<section>
 									<h3>It doesn't get easier than this</h3>
 									<p>
-										First, I build you a beautiful website. Then any time you want something changed, all you have to do is send me an email. I make changes the very same day.</p>
+										I start by working with you to build a brand new website perfectly designed to suit your needs. From there, adding or updating content is as simple as sending me an email. I make most changes that very same day.</br>
+									</p>
 								</section>
 								<section>
 									<h3>I build to your budget</h3>
-									<p>A great online presence doesn't have to cost a fortune. You tell me what your budget is, I build your website for that amount or less.</p>
+									<p>I don't want cost holding your business back from upgrading your online presence. So I let you set the budget. You name the price - I build your website for that amount or less.</p>
 								</section>
 							</div>
 						</div>
@@ -79,12 +80,12 @@
 									<header>
 										<h2>Clean, focused design that<br />
 										looks great on every device</h2>
-										<p>Praesent placerat risus qui</p>
 									</header>
-									<p>Citae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse ac urna. Etiam pellentesque mauris ut lectus. Nunc tellus ante lorem ipsum dolor sit amet consequat. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit orci lorem ipsum dolor sit amet veroeros tempus. Nunc tellus ante lorem ipsum dolor sit amet consequat.</p>
-									<ul class="actions">
-										<li><a href="#" class="button">Magna etiam</a></li>
-									</ul>
+									<p>
+										These days, most people are accessing websites from multiple different devices. It's crucial that your site looks great on all of them. That's why every site I build is designed to deliver content perfect for any device.</br></br>
+										I also work with you to determine what you want your customers to do once they get to your website. Trying to direct people to your products? We'll make that the focus. Prefer that they reach out to you in-person? We'll make sure your contact information is front and center. </br></br>
+										Your website should have a well-designed purpose that helps grow your business. I make sure that happens.
+									</p>
 								</div>
 							</div>
 						</div>
@@ -95,28 +96,49 @@
 						<div class="container">
 							<header>
 								<h2>The process</h2>
-								<p>Pellentesque fermentum dolor aliquam quam lectus facilisis dolore</p>
+								<p>Here's how I build you the perfect website</p>
 							</header>
 							<div class="row 200%">
 								<div class="4u 12u$(small)">
 									<section class="tree first">
-										<span class="icon major fa-camera-retro"></span>
-										<h3>Cras iaculis</h3>
-										<p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit orci. Aenean dignissim.</p>
+										<span class="icon major fa-list"></span>
+										<h3>1. Outline the project</h3>
+										<p>Here's where we figure out what you want your user's experience to be. We'll decide on basic content and functionality and make sure that we're building you a website that caters directly to your customers. We'll also look at some designs, define what you want your site to look like, and determine what we can build that's within your budget.</p>
 									</section>
 								</div>
 								<div class="4u 12u$(small)">
 									<section class="tree">
 										<span class="icon major fa-coffee"></span>
-										<h3>Fusce pellent</h3>
-										<p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit orci. Aenean dignissim.</p>
+										<h3>2. The design proposal</h3>
+										<p>Then I get to work building you the first draft of the site. I take the ideas we discussed during the outline and put them together into a basic version of your website. You'll have the ability to preview this site and provide feedback before we decide to move forward.</p>
 									</section>
 								</div>
 								<div class="4u$ 12u$(small)">
 									<section class="tree last">
 										<span class="icon major fa-camera-retro"></span>
-										<h3>Cras iaculis</h3>
-										<p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit orci. Aenean dignissim.</p>
+										<h3>3. Contract time</h3>
+										<p>Once you've decided we're heading down the right path, we finalize the budget, deadlines, and deliverables and sign a contract to those terms.</p>
+									</section>
+								</div>
+								<div class="4u$ 12u$(small)">
+									<section class="tree last">
+										<span class="icon major fa-camera-retro"></span>
+										<h3>4. Let's build this thing</h3>
+										<p>Now comes the fun part. I get to work on the production version of your site. Similar to the design proposal, you will have constant access to a live preview and the ability to provide feedback throughout the process.</p>
+									</section>
+								</div>
+								<div class="4u$ 12u$(small)">
+									<section class="tree last">
+										<span class="icon major fa-camera-retro"></span>
+										<h3>5. Time to go live!</h3>
+										<p>Once I've finished building your website and you've given the go ahead, we move everything over to your domain. We work out any hiccups that might come up and do final testing to make sure that you are completely satisfied with your new website.</p>
+									</section>
+								</div>
+								<div class="4u$ 12u$(small)">
+									<section class="tree last">
+										<span class="icon major fa-camera-retro"></span>
+										<h3>6. Ongoing management</h3>
+										<p>From here on out, any changes you'd like to make just requires sending me an email. Want to add another product to your online store? Just send me the details. Hired a new employee and want to update your company photo? Send me the new one. You never have to worry about trying to figure out how to do things. I make it as simple as possible.</p>
 									</section>
 								</div>
 							</div>
@@ -126,22 +148,24 @@
 				<!-- Form -->
 					<section id="four" class="wrapper style">
 						<div class="container">
-							<h2>Ready to get started?</h2>
+							<header>
+								<h2>Ready to get started?</h2>
+							</header>
+								<p>Fill out the form below and I'll be in touch very soon. I'm looking forward to working with you!</p>
 							<form method="post" action="#">
 								<div class="row uniform 50%">
 									<div class="6u 12u$(xsmall)">
-										<input type="text" name="name" id="name" value="" placeholder="Name" />
+										<input type="text" name="name" id="name" value="" placeholder="Your name" />
 									</div>
 									<div class="6u$ 12u$(xsmall)">
-										<input type="email" name="email" id="email" value="" placeholder="Email" />
+										<input type="email" name="email" id="email" value="" placeholder="Your email address" />
 									</div>
 									<div class="12u$">
-										<textarea name="message" id="message" placeholder="Tell me a little about what you're looking for" rows="6"></textarea>
+										<textarea name="message" id="message" placeholder="What can I build for you?" rows="6"></textarea>
 									</div>
 									<div class="12u$">
 										<ul class="actions">
 											<li><input type="submit" value="Send Message" class="special" /></li>
-											<li><input type="reset" value="Reset" /></li>
 										</ul>
 									</div>
 								</div>
@@ -151,11 +175,6 @@
 
 				<!-- Footer -->
 					<footer id="footer">
-						<ul class="major-icons">
-							<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon fa-envelope"><span class="label">Email</span></a></li>
-						</ul>
 						<p class="copyright">&copy; Rocky Eastman. All rights reserved.</p>
 					</footer>
 
